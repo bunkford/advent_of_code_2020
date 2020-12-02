c0e19d25b50011a3fd1d19f1b08d7a45db3c3a73
Duncan Clarke <bunkford@gmail.com>
2020-12-02 15:36:27 -0500

    first commit

---
+++ aoc_2020_01.nim
@@ -0,0 +1,23 @@
+import strutils 
+
+# put input into seq
+proc input(file:string):seq[int] =
+  var input: seq[int]
+  for line in lines file:
+    input.add(line.parseInt)
+  return input
+   
+
+proc solve(input:seq[int]):int =
+  for x in input:
+    for y in input:
+      if x + y == 2020: return x*y
+
+proc solve_3sum(input:seq[int]):int =
+   for x in input:
+     for y in input:
+       for z in input:
+         if x + y + z == 2020: return x*y*z
+
+echo "Answer part 1: " , solve(input("/home/pi/Documents/advent_of_code_2020/aoc_2020_01.txt"))
+echo "Answer part 2: " , solve_3sum(input("/home/pi/Documents/advent_of_code_2020/aoc_2020_01.txt"))
---
+++ aoc_2020_02.nim
@@ -0,0 +1,35 @@
+import strutils 
+
+# put input into seq
+proc input(file:string):seq[seq[string]] =
+  var input: seq[seq[string]]
+  for line in lines file:
+    input.add(line.split(":"))
+  return input
+
+proc solve1(input:seq[seq[string]]):int =
+  var good:int = 0
+  for password in input:
+    var letter = password[0].split(" ")[1]
+    var min = password[0].split(" ")[0].split("-")[0].parseInt
+    var max = password[0].split(" ")[0].split("-")[1].parseInt
+    var how_many = password[1].count(letter)
+    if how_many >= min and how_many <= max:
+      inc(good)
+  return good
+
+
+proc solve2(input:seq[seq[string]]):int =
+  var good:int = 0
+  for password in input:
+    var letter = password[0].split(" ")[1]
+    var pos1 = password[0].split(" ")[0].split("-")[0].parseInt
+    var pos2 = password[0].split(" ")[0].split("-")[1].parseInt
+    
+    if password[1][pos1..pos1] == letter xor password[1][pos2..pos2] == letter:
+      inc(good)
+  return good
+
+
+echo "Answer part 1: ", solve1(input("/home/pi/Documents/advent_of_code_2020/aoc_2020_02.txt"))
+echo "Answer part 2: ", solve2(input("/home/pi/Documents/advent_of_code_2020/aoc_2020_02.txt"))
