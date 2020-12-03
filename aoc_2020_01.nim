b91c36705f1d10c38a87da7becb4a81cb47fa91c
Duncan Clarke <bunkford@gmail.com>
2020-12-03 08:36:43 -0500

    fix indents

--- aoc_2020_01.nim
+++ aoc_2020_01.nim
@@ -14,10 +14,10 @@ proc solve(input:seq[int]):int =
       if x + y == 2020: return x*y
 
 proc solve_3sum(input:seq[int]):int =
-   for x in input:
-     for y in input:
-       for z in input:
-         if x + y + z == 2020: return x*y*z
+  for x in input:
+    for y in input:
+      for z in input:
+        if x + y + z == 2020: return x*y*z
 
 echo "Answer part 1: " , solve(input("./aoc_2020_01.txt"))
 echo "Answer part 2: " , solve_3sum(input("./aoc_2020_01.txt"))
