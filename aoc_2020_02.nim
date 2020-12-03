a2226ed6b23b70bf2ccad3da4a99d1c5a7baf994
Duncan Clarke <bunkford@gmail.com>
2020-12-02 21:33:15 -0500

    switched to regex for extracting data

--- aoc_2020_02.nim
+++ aoc_2020_02.nim
@@ -1,35 +1,32 @@
 import strutils, re
 
-# put input into seq
-proc input(file:string):seq[seq[string]] =
-  var input: seq[seq[string]]
-  for line in lines file:
-    input.add(line.split(":"))
-  return input
 
-proc solve1(input:seq[seq[string]]):int =
+proc solve1(file: string):int =
   var good:int = 0
-  for password in input:
-    var letter = password[0].split(" ")[1]
-    var min = password[0].split(" ")[0].split("-")[0].parseInt
-    var max = password[0].split(" ")[0].split("-")[1].parseInt
-    var how_many = password[1].count(letter)
+  for line in lines file:
+    var p = line.findAll(re"\w+")
+    var min = p[0].parseInt
+    var max = p[1].parseInt
+    var letter = p[2][0]
+    var password = p[3]
+    var how_many = password.count(letter)
     if how_many >= min and how_many <= max:
       inc(good)
   return good
 
 
-proc solve2(input:seq[seq[string]]):int =
+proc solve2(file:string):int =
   var good:int = 0
-  for password in input:
-    var letter = password[0].split(" ")[1][0]
-    var pos1 = password[0].split(" ")[0].split("-")[0].parseInt
-    var pos2 = password[0].split(" ")[0].split("-")[1].parseInt
-    
-    if password[1][pos1] == letter xor password[1][pos2] == letter:
+  for line in lines file:
+    var p = line.findAll(re"\w+")
+    var pos1 = p[0].parseInt
+    var pos2 = p[1].parseInt
+    var letter = p[2][0]
+    var password = p[3]
+    if password[pos1-1] == letter xor password[pos2-1] == letter:
       inc(good)
   return good
 
 
-echo "Answer part 1: ", solve1(input("./aoc_2020_02.txt"))
-echo "Answer part 2: ", solve2(input("./aoc_2020_02.txt"))
+echo "Answer part 1: ", solve1("./aoc_2020_02.txt")
+echo "Answer part 2: ", solve2("./aoc_2020_02.txt")
