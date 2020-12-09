b93c1c91aaa1875452169986b20152c1ce4ecbe5
Duncan Clarke <bunkford@gmail.com>
2020-12-09 10:00:29 -0500

    Day 9 Part 2

--- aoc_2020_09.nim
+++ aoc_2020_09.nim
@@ -6,9 +6,9 @@ proc input(file: string): seq[BiggestUInt] =
 
 proc perms(input:seq[BiggestUInt], e:BiggestUInt):bool =
   # check permutations of two numbers in seq that add to equal e
-  for a in 0 .. input.len - 1:
+  for a in 0 ..< input.len:
     # a is  first didgit
-    for b in 0 .. input.len - 1:
+    for b in 0 ..< input.len:
       if a != b: # can't sum itself
         if input[a] + input[b] == e:
           return true
@@ -21,8 +21,21 @@ proc solve1(input: seq[BiggestUInt], preamble: int): BiggestUInt =
 
         
 
-proc solve2(input: seq[BiggestUInt]): BiggestUInt =
-  discard
+proc solve2(input: seq[BiggestUInt], invalid_step1:BiggestUInt): BiggestUInt =
+  var num:BiggestUInt
+  for a in 0 ..< input.len:
+    num = input[a]
+    var min:BiggestUInt = num
+    var max:BiggestUInt = num
+    for b in a ..< input.len:
+      if b != a:
+        if input[b] < min: min = input[b]
+        if input[b] > max: max = input[b]
+        num = num + input[b]
+        if num == invalid_step1:
+          return min + max
+        if num > invalid_step1:
+          break
 
 echo "Part 1 answer: ", solve1(input("aoc_2020_09.txt"), 25)
-echo "Part 2 answer: ", solve2(input("aoc_2020_09.test"))
+echo "Part 2 answer: ", solve2(input("aoc_2020_09.txt"), solve1(input("aoc_2020_09.txt"), 25))
