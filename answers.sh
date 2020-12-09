f6a66f93a3be1413fd8895baf5afd7da22e78a3d
Duncan Clarke <bunkford@gmail.com>
2020-12-09 11:25:45 -0500

    Added script to time execution of all results

---
+++ answers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+clear
+printf "%b" "\e[4;33mAdvent of Code 2020\e[0m"
+for i in {1..25}; 
+do 
+    if test -f "aoc_2020_0$i" 
+    then 
+        printf "%b" "\n\n\e[1;34mDay $i\e[0m:\n"
+        time ./aoc_2020_0$i
+    fi
+done
