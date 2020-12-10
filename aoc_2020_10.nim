13a81ea893b2b958f9a6c002dbad04d024051faa
Duncan Clarke <bunkford@gmail.com>
2020-12-10 10:15:31 -0500

    Day 10 Part 2

--- aoc_2020_10.nim
+++ aoc_2020_10.nim
@@ -21,4 +21,19 @@ proc solve1(input:seq[int]):int =
   var threes: int = diffs.count(3)
   return ones * threes
 
+proc solve2(input:seq[int]):int64 = 
+  var inp = @[0] & input & @[input.max + 3] #effective joltage rating +  adapter joltage ratings + device built in joltage adapter rating
+  var posibilities = newSeq[int64](inp.len)
+  posibilities[0] = input.min # difference between effective joltage rating and first adapter
+  for i in 1 ..< posibilities.len:
+    posibilities[i] = posibilities[i - 1]
+    if i >= 2 and inp[i] - inp[i - 2] <= 3: # add possible combinations
+      posibilities[i] += posibilities[i - 2]
+    if i >= 3 and inp[i] - inp[i - 3] <= 3: # add possible combinations
+      posibilities[i] += posibilities[i - 3]
+  return posibilities[^1]
+
+
+
 echo "Answer part 1: ", solve1(input("./aoc_2020_10.txt"))
+echo "Answer part 2: ", solve2(input("./aoc_2020_10.txt")) 
