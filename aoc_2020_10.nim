9c9f34c22ce14b6de2673531c984b3635bdf4a04
Duncan Clarke <bunkford@gmail.com>
2020-12-10 11:39:14 -0500

    Refactored Part 2

--- aoc_2020_10.nim
+++ aoc_2020_10.nim
@@ -22,18 +22,15 @@ proc solve1(input:seq[int]):int =
   return ones * threes
 
 proc solve2(input:seq[int]):int64 = 
-  var inp = @[0] & input & @[input.max + 3] #effective joltage rating +  adapter joltage ratings + device built in joltage adapter rating
+  var inp = @[0] & input & @[input.max + 3] #effective joltage rating +  adapter joltage ratings + device built in joltage adapter rating (!! adapter joltage ratings are already sorted)
   var posibilities = newSeq[int64](inp.len)
   posibilities[0] = input.min # difference between effective joltage rating and first adapter
-  for i in 1 ..< posibilities.len:
-    posibilities[i] = posibilities[i - 1]
-    if i >= 2 and inp[i] - inp[i - 2] <= 3: # add possible combinations
-      posibilities[i] += posibilities[i - 2]
-    if i >= 3 and inp[i] - inp[i - 3] <= 3: # add possible combinations
-      posibilities[i] += posibilities[i - 3]
+  for i in 0 ..< posibilities.len:
+    for j in i - 3 ..< i:
+      if j >= 0 and inp[i] <= inp[j] + 3:
+        posibilities[i] += posibilities[j]
+
   return posibilities[^1]
-
-
 
 echo "Answer part 1: ", solve1(input("./aoc_2020_10.txt"))
 echo "Answer part 2: ", solve2(input("./aoc_2020_10.txt")) 
