9a8443179e6c4ccca14db478ff10d8f2141d96ed
Duncan Clarke <bunkford@gmail.com>
2020-12-10 10:57:31 -0500

    Fix for leading zero and days 10 - 25

--- answers.sh
+++ answers.sh
@@ -3,9 +3,9 @@ clear
 printf "%b" "\e[4;33mAdvent of Code 2020\e[0m"
 for i in {1..25}; 
 do 
-    if test -f "aoc_2020_0$i" 
+    if test -f "aoc_2020_$(printf "%02d" $i)" 
     then 
-        printf "%b" "\n\n\e[1;34mDay $i\e[0m:\n"
-        time ./aoc_2020_0$i
+        printf "%b" "\n\n\e[1;34mDay $(printf "%02d" $i)\e[0m:\n"
+        time ./aoc_2020_$(printf "%02d" $i)
     fi
 done
