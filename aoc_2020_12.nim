7591b8936cdcb01f053f65fb3bd73c692f0733d9
Duncan Clarke <bunkford@gmail.com>
2020-12-12 07:07:30 -0500

    Day 12 Part 2

--- aoc_2020_12.nim
+++ aoc_2020_12.nim
@@ -35,8 +35,38 @@ proc solve1(input: seq[string]): int =
   return abs(x) + abs(y)
 
 proc solve2(input: seq[string]): int =
-  discard
+  var x, y, dx, dy, angle: int
 
+  dx = 10
+  dy = 1
+
+  for line in input:
+    var task: char = line[0]
+    var value: int = line[1..line.high].parseInt 
+
+    case task:
+      of 'N':
+        inc(dy, value)
+      of 'S':
+        dec(dy, value)
+      of 'E':
+        inc(dx, value)
+      of 'W':
+        dec(dx, value)
+      of 'L', 'R':
+        var d_rad = degToRad(value.toFloat)
+        var radius = sqrt(dx.toFloat^2 + dy.toFloat^2)
+        if task == 'R':
+          d_rad = d_rad * -1
+        var rad = arctan2(dy.toFloat, dx.toFloat) + d_rad
+        dy = (radius * sin(rad)).toInt
+        dx = (radius * cos(rad)).toInt
+      of 'F':
+        inc(x, dx * value)
+        inc(y, dy * value)
+      else:
+        discard
+  return abs(x) + abs(y)
 
 echo "Part 1 answer: ", solve1(input("./aoc_2020_12.txt"))
-echo "Part 2 answer: ", solve2(input("./aoc_2020_12.test"))
+echo "Part 2 answer: ", solve2(input("./aoc_2020_12.txt"))
