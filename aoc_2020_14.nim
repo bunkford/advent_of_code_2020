2a5cc74212e01f49e5f8fc2dd9d3b6a19f9e62b8
Duncan Clarke <bunkford@gmail.com>
2020-12-14 15:17:12 -0500

    Day 14 Part 2

--- aoc_2020_14.nim
+++ aoc_2020_14.nim
@@ -1,5 +1,5 @@
 
-import strutils, strscans, tables, sequtils
+import strutils, strscans, tables, sequtils, bitops
 
 proc solve1(filename: string): int64 =
   var mask: string
@@ -22,4 +22,78 @@ proc solve1(filename: string): int64 =
   
   return toSeq(memory.values).foldl(a + b)
 
+
+iterator product(s: openArray[int], repeat: Positive): seq[int] =
+
+  var counters = newSeq[int](repeat)
+
+  block outer:
+    while true:
+      var result = newSeq[int](repeat)
+      for i, cnt in counters:
+        result[i] = s[cnt]
+      yield result
+
+      var i = repeat - 1
+      while true:
+        inc counters[i]
+        if counters[i] == s.len:
+          counters[i] = 0
+          dec i
+        else: break
+        if i < 0:
+          break outer
+
+proc solve2(filename: string): int64 =
+  var mask: string
+  var memory = initTable[int64, int64]()
+  for line in filename.lines:
+
+    var mem, num: int
+
+    # set mask
+    if line[0..3] == "mask":
+      mask = line.split(" = ")[1]
+    
+    elif scanf(line, "mem[$i] = $i", mem, num):
+      var n = mem.toBin(36)
+      for k, d in mask:
+          if d == '0': continue
+          n[k] = d
+
+      # need to get all combinations of 0's and 1'
+      var s = @['0', '1'] 
+      var repeat = mask.count("X")
+      var counters = newSeq[int](repeat)
+
+      block outer:
+        while true:
+          var r = newSeq[char](repeat)
+          for i, cnt in counters:
+            r[i] = s[cnt]
+
+          # r now contains a variation 
+          var t = n
+          for i in 0..n.high:
+            if t[i] == 'X':
+              t[i] = r.pop()
+          
+          memory[t.fromBin[:int64]] = num
+
+          var i = repeat - 1
+          while true:
+            inc counters[i]
+            if counters[i] == s.len:
+              counters[i] = 0
+              dec i
+            else: break
+            if i < 0:
+              break outer
+
+
+  return toSeq(memory.values).foldl(a + b)
+
+
 echo "Answer part 1: ", solve1("./aoc_2020_14.txt") 
+
+echo "Answer part 2: ", solve2("./aoc_2020_14.txt")
